787b1f5be12341c0e115da86c7f9a7d99e45db76
Mackev <mn.kevin31@gmail.com>
2022-08-07 07:08:46 +0200

    :iphone: Make navbar and hero section responsive

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,57 +1,73 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import HTMLTag from "../HTMLTag/HTMLTag";
 import "./HeroSection.css";
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 
 function HeroSection() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, [width]);
+
   return (
     <div className="hero__container">
       <div className="hero">
         <div className="hero__text">
-          <h1 className="hero-title">
-            <HTMLTag name="h1" className="hero-title">
+          <HTMLTag name="h1" className="hero-title">
+            <h1 className="hero-title">
               Hello,<br/>
               My name is <strong>Macé Kevin</strong>
-            </HTMLTag>
-          </h1>
-          <p>
-            <HTMLTag name="p">
+            </h1>
+          </HTMLTag>
+          <HTMLTag name="p">
+            <p>
               I am a french self-taught developer interested in <strong>programming</strong> and <strong>new technologies</strong>
-            </HTMLTag>
-          </p>
+            </p>
+          </HTMLTag>
         </div>
-        <div className="hero__logo-container">
-          <div className="hero__logo">
-            <Logo />
-          </div>
-          <div className="inside-spin">
-            <div className="inside-arc inside-arc-left"></div>
-            <div className="inside-arc inside-arc-right"></div>
+        {
+          width <= 1185 ? "" :
+          <div className="hero__logo-container">
+            <div className="hero__logo">
+              <Logo />
+            </div>
+            <div className="inside-spin">
+              <div className="inside-arc inside-arc-left"></div>
+              <div className="inside-arc inside-arc-right"></div>
 
-            <div className="inside-ball inside-ball-left__start">
-              <div></div>
+              <div className="inside-ball inside-ball-left__start">
+                <div></div>
+              </div>
+              <div className="inside-ball inside-ball-left__end">
+                <div></div>
+              </div>
+              <div className="inside-ball inside-ball-right__start">
+                <div></div>
+              </div>
+              <div className="inside-ball inside-ball-right__end">
+                <div></div>
+              </div>
             </div>
-            <div className="inside-ball inside-ball-left__end">
-              <div></div>
-            </div>
-            <div className="inside-ball inside-ball-right__start">
-              <div></div>
-            </div>
-            <div className="inside-ball inside-ball-right__end">
-              <div></div>
-            </div>
-          </div>
-          <div className="outside-spin">
-            <div className="outside-arc"></div>
+            <div className="outside-spin">
+              <div className="outside-arc"></div>
 
-            <div className="outside-ball outside-ball__start">
-              <div></div>
-            </div>
-            <div className="outside-ball outside-ball__end">
-              <div></div>
+              <div className="outside-ball outside-ball__start">
+                <div></div>
+              </div>
+              <div className="outside-ball outside-ball__end">
+                <div></div>
+              </div>
             </div>
           </div>
-        </div>
+        }
       </div>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Navbar.css";
 
 function Navbar() {
+  const [menu, setMenu] = useState(false);
+
+  const handleClick = () => {
+    setMenu(!menu);
+  }
+
   return (
     <header className="header">
-      <nav className="navbar">
+      <nav className={`navbar${menu ? ' menu-open' : ''}`}>
         <ul>
           <li><a href="#" className="nav-link">&lt;Home /&gt;</a></li>
           <li><a href="#" className="nav-link">&lt;About /&gt;</a></li>
           <li><a href="#" className="nav-link">&lt;Projects /&gt;</a></li>
           <li><a href="#" className="nav-link">&lt;Contact /&gt;</a></li>
         </ul>
+        <button className="navbar-menu" onClick={handleClick}>
+          <span>&lt;{menu ? "Close" : "Open"} /&gt;</span>
+        </button>
       </nav>
     </header>
   );
